8e99e75bfd9be332afb1b9771dd129c729b13e4a
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-12 14:42:10 +0530

    added , delted and updated the file

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -4,5 +4,8 @@ public class HomePage {
 
 	public void test1() {
 		System.out.println("test1 homepage-- LP");
+		
+		String username = "abc";
+		String Password ="abc";
 	}
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+public class SearchPage {
+
+	public void test1() {
+		System.out.println("test1 Searchpage-- LP");
+		
+	}
+}
+
