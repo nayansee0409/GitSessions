c8708a3851418a13a3d23e651dff385f57100030
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-12 16:45:14 +0530

    added registration page class

---
+++ src/pages/RegistrationPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+public class RegistrationPage {
+
+	
+	public void test1() {
+		System.out.println("test1 Searchpage-- LP");
+		
+		System.out.println("test2 Searchpage-- LP");
+		
+	}
+	
+}
