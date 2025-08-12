ce146f2fe1a08fb77dca55f0c200c73c4cf4520f
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-12 21:29:15 +0530

    added lines of code

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,8 @@ public class LoginPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s = "Login page";
+		System.out.println("test login page -- LP" + s);
 
 	}
 
