1d6f92a3f416124df3d2f098dbeffee870da9961
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-13 10:24:28 +0530

    c1

--- src/pages/ClassicPage.java
+++ src/pages/ClassicPage.java
@@ -4,26 +4,19 @@ public class ClassicPage {
 
 	public static void main(String[] args) {
 
-
-		int b= 10;
-		int c= 30;
+		int a = 10;
+		int b = 10;
+		int c = 30;
 		int d = 40;
+		int e = 40;
 		
-
-	  int a =10;
-	
 
 		System.out.println("F1 version");
-		
 
 		System.out.println("Classic page is important");
 
-		
-		
-		
-		if (true)
-		{
-			System.out.println("Classic page is important after merge confilict ");	
+		if (true) {
+			System.out.println("Classic page is important after merge confilict ");
 
 		}
 
