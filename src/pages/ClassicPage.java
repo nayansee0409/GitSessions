0f22dca3bee61c73ed44e1913d0cc2e400fe7105
nayansee0409 <nayanseesuhane0409@gmail.com>
2025-08-13 09:19:36 +0530

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("F1 version");
+		
+		if (true)
+		{
+			System.out.println("Classic page is important");	
+
+		}
+
+	}
+}
