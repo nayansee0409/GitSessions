ad186e46cbcc8db3cb7fb1622223a53371a8a7f7
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-13 10:30:02 +0530

    added changes in feature filE

--- src/pages/FeaturePage.java
+++ src/pages/FeaturePage.java
@@ -5,6 +5,7 @@ public class FeaturePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	String F1 = "feature";
 		System.out.println("F1 version");
 		
 		System.out.println("F2 version");
