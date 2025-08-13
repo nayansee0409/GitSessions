b576b1a62671646e36c6eec03899074814b6af11
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-13 07:31:27 +0530

    second cherry pick case

--- src/pages/CherryPick.java
+++ src/pages/CherryPick.java
@@ -5,13 +5,16 @@ public class CherryPick {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	System.out.println("F1 version");
-		
-		System.out.println("F2 version");
-		
-		System.out.println("F3 version");
-	}
+		System.out.println("F1 version");
 
-	
+		System.out.println("F2 version");
+
+		System.out.println("F3 version");
+
+		System.out.println("r2 version");
+
+		System.out.println("r3 version");
+
+	}
 
 }
