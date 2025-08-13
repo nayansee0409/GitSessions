14fa09eb1561feaa9437935286f1fcb54e8ed785
nayansee0409 <nayanseesuhane0409@gmai.com>
2025-08-13 09:40:34 +0530

    added few variables

--- src/pages/ClassicPage.java
+++ src/pages/ClassicPage.java
@@ -4,11 +4,18 @@ public class ClassicPage {
 
 	public static void main(String[] args) {
 
+		int b= 10;
+		int c= 30;
+		int d = 40;
+		
 		System.out.println("F1 version");
 		
 
 		System.out.println("Classic page is important");
 
+		
+		
+		
 		if (true)
 		{
 			System.out.println("Classic page is important after merge confilict ");	
